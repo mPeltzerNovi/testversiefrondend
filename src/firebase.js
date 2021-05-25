8f3ebb8bb5961dc13765a02536dc2c1ede768301
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-05-25 14:55:51 +0200

    Let op: firebase.js aangepast nav het spoedig verlopen van de firebase-database eind mei.
    Het nieuwe gmail-account is:
    judithfirebasenovibootcamp3@gmail.com
    
    Het nieuwe wachtwoord is het oude wachtwoord uit de installatiehandleiding aangevuld met: "mei21".
    
    Dus: "XXXXXXXXmei21"

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 
-
-const firebaseConfig = {
+//Let op dit is de code voor de eerste firebase-database. Omdat deze verliep moest ik een nieuwe maken. Zie onder
+/*const firebaseConfig = {
     apiKey: "AIzaSyCe_m5lDWb8efYy6oO3dxihrDIJHg5Wy7Y",
     authDomain: "messagesender-56cd4.firebaseapp.com",
     projectId: "messagesender-56cd4",
@@ -9,6 +9,17 @@ const firebaseConfig = {
     messagingSenderId: "793772570877",
     appId: "1:793772570877:web:65d172b4d680140b9a6694",
     measurementId: "G-2WGF1KGSEC"
+};*/
+
+//Nieuwe firebase-database ivm verlopen eerste database eind mei
+const firebaseConfig = {
+    apiKey: "AIzaSyAxHRcEUcm_OszbrRibJXzkv9RW51YywYw",
+    authDomain: "judith-ac214.firebaseapp.com",
+    projectId: "judith-ac214",
+    storageBucket: "judith-ac214.appspot.com",
+    messagingSenderId: "431388111925",
+    appId: "1:431388111925:web:8d938c8937737da1176d7c",
+    measurementId: "G-1VFTJJV58J"
 };
 
 //connect react frondend to firebase backend:
